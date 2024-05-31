bb8a115d3d3270b075029c6df52e84d40abd098f
Vrutang-Shah <102398104+Vrutang-Shah@users.noreply.github.com>
2024-05-31 12:58:39 +0000

    First Practice Commit

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -6,7 +6,14 @@ name = "John"
 intrupution = f"Hello {name}"
 
 greet_format = "Hello {}"
+print(greet_format)
 
+name = "Vrut"
 formatted = greet_format.format(name)
+print(formatted)
 
-print(intrupution, formatted)
+print(intrupution, formatted, "you")
+
+name = "vrutang"
+print(f"Vrutang {name:^20}")
+
